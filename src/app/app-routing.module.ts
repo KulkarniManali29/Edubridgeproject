e0900c316023656fbeb002a061b8b257c59ef4a6
KulkarniManali29 <kulkarnim997@gmail.com>
2023-04-05 23:26:10 +0530

    Angularproject

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { HeaderComponent } from './header/header.component';
+import { PaymentComponent } from './payment/payment.component';
+import { ProductComponent } from './product/product.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'register-user',component:RegisterUserComponent},
+  {path:'',redirectTo:'header',pathMatch:'full'},
+
+  {path:'product',component:ProductComponent},
+  {path:'cart',component:CartComponent},
+  {path: 'checkout',component:CheckoutComponent},
+  {path:'payment',component:PaymentComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
